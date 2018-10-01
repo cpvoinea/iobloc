a98aac31d5e575e25a991a53037a61798ebab574
Catalin Voinea <cpvoinea@gmail.com>
2018-10-01 16:24:40 +0300

    restore Program.cs

--- Program.cs
+++ Program.cs
@@ -8,53 +8,50 @@ namespace iobloc
 
         static void Main(string[] args)
         {
-            Console.WriteLine(0x30);
-            Console.WriteLine((char)0x30);
-            Console.ReadKey();
-            // for (int i = 0; i < _list.Length; i++)
-            //     Console.WriteLine("{0}: {1}", i, _list[i]);
-            // int key = Console.ReadKey().KeyChar - '0';
-            // if (key < 0 || key >= _list.Length)
-            //     return;
+            for (int i = 0; i < _list.Length; i++)
+                Console.WriteLine("{0}: {1}", i, _list[i]);
+            int key = Console.ReadKey().KeyChar - '0';
+            if (key < 0 || key >= _list.Length)
+                return;
 
-            // Game game = null;
-            // try
-            // {
-            //     IBoard board = null;
-            //     switch (key)
-            //     {
-            //         case 0:
-            //             board = new TetrisBoard();
-            //             break;
-            //         case 1:
-            //             board = new RunnerBoard();
-            //             break;
-            //         case 2:
-            //             board = new HelicopterBoard();
-            //             break;
-            //         case 3:
-            //             board = new BreakoutBoard();
-            //             break;
-            //     }
+            Game game = null;
+            try
+            {
+                IBoard board = null;
+                switch (key)
+                {
+                    case 0:
+                        board = new TetrisBoard();
+                        break;
+                    case 1:
+                        board = new RunnerBoard();
+                        break;
+                    case 2:
+                        board = new HelicopterBoard();
+                        break;
+                    case 3:
+                        board = new BreakoutBoard();
+                        break;
+                }
 
-            //     game = new Game(board);
-            //     game.Ended += GameEnded;
-            //     game.Start();
-            // }
-            // catch (Exception ex)
-            // {
-            //     Console.Clear();
-            //     Console.WriteLine(ex);
-            //     Console.ReadKey(true);
-            // }
-            // finally
-            // {
-            //     if (game != null)
-            //     {
-            //         game.Ended -= GameEnded;
-            //         game.Close();
-            //     }
-            // }
+                game = new Game(board);
+                game.Ended += GameEnded;
+                game.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine(ex);
+                Console.ReadKey(true);
+            }
+            finally
+            {
+                if (game != null)
+                {
+                    game.Ended -= GameEnded;
+                    game.Close();
+                }
+            }
         }
 
         static void GameEnded(object sender, EventArgs ea)
