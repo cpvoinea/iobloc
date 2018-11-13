f7feb473103330f0cc1dd625a735b86c98dce09e
cpvoinea <cpvoinea@gmail.com>
2018-11-13 20:56:18 +0200

    version change

--- publish.bat
+++ publish.bat
@@ -1,1 +1,2 @@
-dotnet nuget push iobloc.2.5.0.nupkg -k nuget_key_from_email -s https://api.nuget.org/v3/index.json
+dotnet pack
+dotnet nuget push bin\Debug\iobloc.2.5.2.nupkg -k nuget_key_from_email -s https://api.nuget.org/v3/index.json
